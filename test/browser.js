15640ccfa9055d62c328899aefd960b4e4142de5
Matthew Dapena-Tretter <m@tthewwithanm.com>
2014-05-30 06:00:28 -0400

    Test that titles are updated as expected

--- test/browser.js
+++ test/browser.js
@@ -1,8 +1,41 @@
-var assert = chai.assert;
+var assert = chai.assert,
+    html = React.DOM.html,
+    title = React.DOM.title,
+    FrozenHead = ReactFrozenHead;
 
+var page = function (pageTitle) {
+    return (
+        html(null,
+            FrozenHead(null,
+                title(null, pageTitle)
+            )
+        )
+    );
+};
+
+var App = React.createClass({
+    getDefaultProps: function () {
+        return {title: 'HOME'};
+    },
+    render: function () {
+        return page(this.props.title);
+    }
+});
 
 describe('react-frozenhead', function () {
-    it('passes a test', function () {
-        assert(true);
+    it('renders the initial title', function () {
+        var app = App({title: 'INITIAL'}),
+            html = React.renderComponentToString(app);
+        assert.include(html, '<title>INITIAL</title>');
+    });
+
+    it("doesn't update the DOM title after mounting", function (done) {
+        var container = document.createElement('div');
+            app = React.renderComponent(App({title: 'INITIAL'}), container);
+        app.setProps({title: 'NEW'}, function () {
+            var titleEl = container.querySelector('title');
+            assert.equal(titleEl.text, 'INITIAL');
+            done();
+        });
     });
 });
